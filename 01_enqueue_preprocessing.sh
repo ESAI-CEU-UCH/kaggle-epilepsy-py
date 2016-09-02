97c41ba1c2c1b8376650d115480d7eee2982c17e
Paco Zamora Martinez <pakozm@gmail.com>
2016-09-02 10:52:24 +0200

    Added license disclaimers

--- 01_enqueue_preprocessing.sh
+++ 01_enqueue_preprocessing.sh
@@ -1,5 +1,5 @@
- #!/bin/bash
-/home/experimentos/HERRAMIENTAS/bin/qsub-wrapper.sh --omp=4 -- python scripts/PREPROCESS/compute_fft_60s_30s_BFPLOS_Dogs.py
-/home/experimentos/HERRAMIENTAS/bin/qsub-wrapper.sh --omp=4 -- python scripts/PREPROCESS/compute_fft_60s_30s_BFPLOS_Patients.py
-/home/experimentos/HERRAMIENTAS/bin/qsub-wrapper.sh --omp=4 -- python scripts/PREPROCESS/compute_fft_60s_30s_compress_Dogs.py
-/home/experimentos/HERRAMIENTAS/bin/qsub-wrapper.sh --omp=4 -- python scripts/PREPROCESS/compute_fft_60s_30s_compress_Patients.py
+#!/bin/bash
+OMP_NUM_THREADS=4 /home/experimentos/HERRAMIENTAS/bin/qsub-wrapper.sh -l nodes=1:ppn=4:CPD -- python scripts/PREPROCESS/compute_fft_60s_30s_BFPLOS_Dogs.py
+OMP_NUM_THREADS=4 /home/experimentos/HERRAMIENTAS/bin/qsub-wrapper.sh -l nodes=1:ppn=4:CPD -- python scripts/PREPROCESS/compute_fft_60s_30s_BFPLOS_Patients.py
+OMP_NUM_THREADS=4 /home/experimentos/HERRAMIENTAS/bin/qsub-wrapper.sh -l nodes=1:ppn=4:CPD -- python scripts/PREPROCESS/compute_fft_60s_30s_compress_Dogs.py
+OMP_NUM_THREADS=4 /home/experimentos/HERRAMIENTAS/bin/qsub-wrapper.sh -l nodes=1:ppn=4:CPD -- python scripts/PREPROCESS/compute_fft_60s_30s_compress_Patients.py
--- scripts/PREPROCESS/compute_fft_60s_30s_compress_Patients.py
+++ scripts/PREPROCESS/compute_fft_60s_30s_compress_Patients.py
@@ -1,3 +1,26 @@
+# This file is part of ESAI-CEU-UCH/kaggle-epilepsy-py (https://github.com/ESAI-CEU-UCH/kaggle-epilepsy-py)
+#
+# Copyright (c) 2016, ESAI, Universidad CEU Cardenal Herrera,
+# (F. Zamora-Martinez, F. Mu\~noz-Malmaraz, J. Pardo)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
 work_dir = "/home/experimentos/KAGGLE/EPILEPSY_PREDICTION_IN_PYTHON/kaggle-epilepsy-py/"
 data_dir = "/home/experimentos/CORPORA/KAGGLE/EPILEPSY_PREDICTION/"
 out_dir = "/home/experimentos/KAGGLE/EPILEPSY_PREDICTION_IN_PYTHON/FFT_60s_30s_COMPRESS/"
