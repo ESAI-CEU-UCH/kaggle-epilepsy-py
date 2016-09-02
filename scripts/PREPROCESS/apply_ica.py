359fe4deac4e2949096ce83c5a5cba29af025119
Paco Zamora Martinez <pakozm@gmail.com>
2016-09-02 12:33:35 +0200

    Added ICA preprocessing scripts

---
+++ scripts/PREPROCESS/apply_ica.py
@@ -0,0 +1,62 @@
+# This file is part of ESAI-CEU-UCH/kaggle-epilepsy-py (https://github.com/ESAI-CEU-UCH/kaggle-epilepsy-py)
+#
+# Copyright (c) 2016, ESAI, Universidad CEU Cardenal Herrera,
+# (F. Zamora-Martinez, F. Mu\~noz-Malmaraz, J. Pardo)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+WORK_DIR      = "/home/experimentos/KAGGLE/EPILEPSY_PREDICTION_IN_PYTHON/kaggle-epilepsy-py/"
+FFT_DATA_PATH = "/home/experimentos/KAGGLE/EPILEPSY_PREDICTION_IN_PYTHON/FFT_60s_30s_BFPLOS/"
+ICA_DATA_PATH = "/home/experimentos/KAGGLE/EPILEPSY_PREDICTION_IN_PYTHON/FFT_60s_30s_BFPLOS/"
+OUTPUT_PATH   = "/home/experimentos/KAGGLE/EPILEPSY_PREDICTION_IN_PYTHON/FFT_60s_30s_BFPLOS_PCA/"
+SUBJECTS      = [ "Dog_1", "Dog_2", "Dog_3", "Dog_4", "Dog_5", "Patient_1", "Patient_2" ]
+NUM_CORES     = 4
+
+import sys
+sys.path.append(WORK_DIR)
+import ESAIEpilepsyLib as elib
+from glob import glob
+from multiprocessing import Pool
+import numpy as np
+import os
+
+os.chdir(WORK_DIR)
+elib.mkdir(OUTPUT_PATH)
+
+pool = Pool(processes=4)
+
+for subject in SUBJECTS:
+    print "# " + subject
+    center = np.loadtxt("%s/%s_ica_center.txt"%(ICA_DATA_PATH, subject))
+    K = np.loadtxt("%s/%s_ica_K.txt"%(PCA_DATA_PATH, subject))
+    W = np.loadtxt("%s/%s_ica_W.txt"%(PCA_DATA_PATH, subject))
+    center = center.squeeze()
+    def transform(x): return np.matmul(np.matmul(np.multiply(x - center, K), W)
+    files = glob("%s/%s*channel_01*"%(FFT_DATA_PATH, subject))
+    assert len(files) > 0
+    def f(filename):
+        mask = filename.replace("channel_01", "channel_??")
+        outname = "%s/%s.csv.gz"%(OUTPUT_PATH,
+                                  os.path.basename(filename).replace(".channel_01.csv.gz",""))
+        if not os.path.exists(outname):
+            m = np.concatenate([ np.loadtxt(x) for x in sorted(glob(mask)) ], axis=1)
+            out = transform(m)
+            np.savetxt(outname, out, delimiter=' ', fmt='%.5g')
+        
+    result = pool.map(f, files)
---
+++ scripts/PREPROCESS/compute_ica.R
@@ -0,0 +1,95 @@
+# This file is part of ESAI-CEU-UCH/kaggle-epilepsy (https://github.com/ESAI-CEU-UCH/kaggle-epilepsy)
+# and ESAI-CEU-UCH/kaggle-epilepsy-py (https://github.com/ESAI-CEU-UCH/kaggle-epilepsy-py)
+#
+# Copyright (c) 2014, ESAI, Universidad CEU Cardenal Herrera,
+# (F. Zamora-Martínez, F. Muñoz-Malmaraz, P. Botella-Rocamora, J. Pardo)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+library(fastICA)
+library(stringr)
+
+SEED <- 37442
+subjects <- c("Dog_1", "Dog_2", "Dog_3", "Dog_4", "Dog_5", "Patient_1", "Patient_2")
+sources <- "/home/experimentos/KAGGLE/EPILEPSY_PREDICTION_IN_PYTHON/FFT_60s_30s_BFPLOS"
+dest <- "/home/experimentos/KAGGLE/EPILEPSY_PREDICTION_IN_PYTHON/FFT_60s_30s_BFPLOS"
+
+files <- dir(sources)
+
+getChannelNumber <- function(path) {
+    as.numeric(gsub("^.*channel_(..).*$", "\\1", path))
+}
+
+readSet <- function(auxfiles, seg, nlen, nfft, nfilt, nchannels) {
+    data <- array(dim=c(nlen,nfft,nfilt,nchannels))
+    for (i in 1:nlen){
+        for (j in 1:nchannels){
+            path <- paste(sources, auxfiles[seg[(i-1)*nchannels+j]], sep="/")
+            chan <- getChannelNumber(path)
+            tt <- read.table(path)
+            data[i,,,chan] <- as.matrix(tt)
+        }
+    }
+    data
+}
+
+for (hh in 1:length(subjects)){
+    subject <- subjects[hh]
+    write(paste("#",subject), stdout())
+    output.center <- paste(dest, "/", subject, "_ica_center.txt", sep="")
+    output.K <- paste(dest, "/", subject, "_ica_K.txt", sep="")
+    output.W <- paste(dest, "/", subject, "_ica_W.txt", sep="")
+    if (!file.exists(output.center) || !file.exists(output.K) ||
+        !file.exists(output.W)) {
+        auxfiles <- files[grep(subject,files)]
+
+        trseg <- grep("ictal",auxfiles)
+        
+        nchannels <- length(trseg) / length(grep("channel_01",
+                                                 auxfiles[trseg]))
+        
+        ntraining <- length(trseg)/nchannels 
+        
+        tt <- read.table(paste(sources, auxfiles[trseg[1]], sep="/"))
+        nfft <- dim(tt)[1]
+        nfilt <- dim(tt)[2]
+        
+                                        # Read all training data
+        data <- readSet(auxfiles, trseg, ntraining, nfft, nfilt, nchannels)
+        
+                                        # Decompose and align by minute all the data
+        data1 <- (apply(apply(data,c(2,1),unlist),1,unlist))
+
+                                        # Compute centers
+        center <- colMeans(data1)
+        
+                                        # Compute ICA
+        set.seed(SEED)
+        ica <- fastICA(data1,n.comp=dim(data1)[2],method="C")
+
+                                        # Write transformation matrices
+        write.table(center, file = output.center,
+                    sep = " ", col.names = FALSE, row.names = FALSE)
+        write.table(ica$K, file = output.K,
+                    sep = " ", col.names = FALSE, row.names = FALSE)
+        write.table(ica$W, file = output.W,
+                    sep = " ", col.names = FALSE, row.names = FALSE)
+
+    }
+}
